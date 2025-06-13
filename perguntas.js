09e8d9fde6cfac8b12060cc998b5982bacad74a4
Cleberbalafina <167100027+Cleberbalafina@users.noreply.github.com>
2025-06-13 10:19:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,48 @@
+criaCartao(
+    'Odontologia',
+    'Quantos dentes o bluezao tem na boca?',
+    'Nenhum, os dentes estão nascendo XD'
+)
+
+criaCartao(
+    'Economia',
+    'Quantos pesos argentinos o ronaldinho ganhou?',
+    '1000'
+)
+
+criaCartao(
+    'Famosos',
+    'Qual a antiga profissão do Clovis Basilio do Santos?',
+    'Ator de filmes homoeróticos'
+)
+
+criaCartao(
+    'Culinária invertebrada',
+    'Qual a cor da lagarta tóxica que o bluezao comeu?',
+    'Verde tóxico'
+)
+criaCartao(
+    'Caridade',
+    'Quanto centavos o pobre miserável enviou para Luangameolays?',
+    '9 centavos (morra miserável)'
+)
+criaCartao(
+    'Politíca',
+    'Qual o politíco favorito de Ronaldo Cabral?',
+    'Alexandre de Moraes (Xandão) UwU'
+)
+criaCartao(
+    'Religião',
+    'O que o mendigo bluezao come após encontrar na rua?',
+    'Pipoca e suspiro de macumba kkkkkkkkkkkk'
+)
+criaCartao(
+    'Medicina',
+    'Qual a doença óssea que Ronaldo Cabral possui?',
+    'Deficiência óssea (Por um milagre ele está vivo) :3'
+)
+criaCartao(
+    'Comédia',
+    'Surdo e mudo com parkinson, pode ser considerado gago?',
+    'Sim ;p (aguarde 8 anos de cadeia)'
+)
